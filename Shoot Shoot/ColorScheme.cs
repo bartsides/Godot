488a871d21ccf365eaedd3015578e8b723e123a5
themightygwar <37585072+themightygwar@users.noreply.github.com>
2022-10-09 14:56:11 -0500

    Pink Bunny Idle
    Pink Bunny 32 px Idle Animation
    Green Knight Walk Cycle + Hit Animation

--- Shoot Shoot/ColorScheme.cs
+++
@@ -1,9 +0,0 @@
-using System.Drawing;
-
-public struct ColorScheme {
-    public Color Outline;
-    public Color Top;
-    public Color Light;
-    public Color Dark;
-    public Color Void;
-}
---
+++ animations/Bouncy/Shoot Shoot/ColorScheme.cs
@@ -0,0 +1,9 @@
+using System.Drawing;
+
+public struct ColorScheme {
+	public Color Outline;
+	public Color Top;
+	public Color Light;
+	public Color Dark;
+	public Color Void;
+}
